822217fedabd32be4274c1131a3ebf5bd6aba5f6
hakan sonmez <hakan.sn.hs@gmail.com>
2020-07-15 18:09:42 +0300

    Start implementing vm

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
-
-#include <stdio.h>
+#include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
+  
   Chunk chunk;
   initChunk(&chunk);  
- 
-  // int l[] = {1,2,3,4};
-  // chunk.lines = l; 
-  // chunk.lastLine = 4;
-
-  for (int i = 0; i < 256; i++) {
-    writeConstant(&chunk, i, 4);
-  }
-  writeConstant(&chunk, 5.6, 5);
-
-  writeChunk(&chunk, OP_RETURN, 4);  
-  dissassembleChunk(&chunk, "test_chunk");
-  freeChunk(&chunk);  
+  int constant = addConstant(&chunk, 1.2);
   
+  writeChunk(&chunk, OP_CONSTANT, 4);
+  writeChunk(&chunk, constant, 4);
+  writeChunk(&chunk, OP_RETURN, 4);
+  dissassembleChunk(&chunk, "test_chunk");
+  interpret(&chunk);
+  freeVM();
+  freeChunk(&chunk);  
+
   return 0;
 }
